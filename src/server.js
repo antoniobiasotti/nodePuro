c99cf24375a43f78cf07ccb3838446142e2118c2
Antonio <abamorim36@gmail.com>
2023-09-30 13:31:46 -0300

    JSON Response

--- src/server.js
+++ src/server.js
@@ -1,14 +1,25 @@
 import http from 'node:http'
 // import fastify from 'fastify
 
+const users = []
+
 const server = http.createServer((req, res) => {
      const { method, url } = req;
 
+     console.log(req.headers);
+
      if (method === 'GET' && url === '/users') {
-          return res.end('Listagem de usuários')
+          return res
+          .setHeader('Content-type', 'application/json')
+          .end(JSON.stringify(users))
      }
 
      if (method === 'POST' && url === '/users') {
+          users.push({
+               id: 1,
+               name: 'John Doe',
+               email: 'johndoe@example.com',
+          })
           return res.end('Criação de usuário')
      }
 

7ec1c9f90f035fa123f7fbb4bf775d21804794cf
Antonio <abamorim36@gmail.com>
2023-09-30 11:51:04 -0300

    Created Basic Routing

--- src/server.js
+++ src/server.js
@@ -2,6 +2,16 @@ import http from 'node:http'
 // import fastify from 'fastify
 
 const server = http.createServer((req, res) => {
+     const { method, url } = req;
+
+     if (method === 'GET' && url === '/users') {
+          return res.end('Listagem de usuários')
+     }
+
+     if (method === 'POST' && url === '/users') {
+          return res.end('Criação de usuário')
+     }
+
      return res.end('Hello World')
 })
 

a0ff6a3f2da29448c79f1584e2a47d36c0336f86
Antonio <abamorim36@gmail.com>
2023-10-02 05:44:10 -0300

    Writable and Transformation Streams

--- streams/fundamental.js
+++ streams/fundamental.js
@@ -1,7 +1,7 @@
 /* process.stdin
      .pipe(process.stdout) */
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
      index = 1
@@ -22,4 +22,21 @@ class OneToHundredStream extends Readable {
      }
 }
 
-new OneToHundredStream().pipe(process.stdout)
+class MultiplyByTenStream extends Writable {
+     _write(chunk, enconding, callback) {
+          console.log(Number(chunk.toString()) * 10)
+          callback()
+     }
+}
+
+class InverseNumberStream extends Transform {
+     _transform(chunk, enconding, callback) {
+          const tranformed = Number(chunk.toString()) * -1
+
+          callback(null, Buffer.from(String(tranformed)))
+     }
+}
+
+new OneToHundredStream()
+     .pipe(new InverseNumberStream())
+     .pipe(new MultiplyByTenStream())

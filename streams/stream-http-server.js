32ebdb1212dac88d639ec28fd2b32dfff4c71d78
Antonio <abamorim36@gmail.com>
2023-10-02 08:02:44 -0300

    Await Complete Stream

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -11,10 +11,22 @@ class InverseNumberStream extends Transform {
      }
 }
 
-const server = http.createServer((req, res) => {
-     return req
+const server = http.createServer(async (req, res) => {
+     const buffers = []
+
+     for await (const chunk of req) {
+          buffers.push(chunk)
+     }
+
+     const fullStreamContent = Buffer.concat(buffers).toString()
+
+     console.log(fullStreamContent)
+
+     return res.end(fullStreamContent)
+
+  /*    return req
           .pipe(new InverseNumberStream())
-          .pipe(res)
+          .pipe(res) */
 })
 
 server.listen(3334)

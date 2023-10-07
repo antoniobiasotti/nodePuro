71c098e85022a8a31ebc54b5227cbb4e30c8a7cf
Antonio <abamorim36@gmail.com>
2023-10-07 05:13:42 -0300

    Importing randomUUID()

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from '../middlewares/json.js';
 import { Database } from './database.js';
 // import fastify from 'fastify
@@ -31,7 +32,7 @@ const server = http.createServer(async (req, res) => {
           const { name, email } = req.body
 
           const user = {
-               id: 1,
+               id: randomUUID(),
                name,
                email,
           }

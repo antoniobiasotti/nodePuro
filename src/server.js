2a46ffd2e1f0ae0e1228e8726d22929dfa2baad5
Antonio <abamorim36@gmail.com>
2023-10-11 07:28:52 -0300

    Removing a User

--- src/server.js
+++ src/server.js
@@ -12,12 +12,16 @@ const server = http.createServer(async (req, res) => {
      await json(req, res)
 
      const route = routes.find(route => {
-          return route.method === method && route.path === url
+          return route.method === method && route.path.test(url)
      })
 
      console.log(route)
 
      if (route) {
+          const routeParams = req.url.match(route.path)
+
+          req.params = { ...routeParams.groups}
+
           return route.handler(req, res)
      }
 
